afba55ef221a07d5d4e5c4d1d48a2075f37a2d4a
imaha7 <ilham.maulana.07.0698@gmail.com>
2026-05-22 14:58:47 +0700

    Enhancement SEO

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://imaha7-profile.vercel.app/sitemap.xml",
+  };
+}
+
+
